b8f1207d564b99347095d8a6fd8b003e1f48ac55
T.Ando <taizo.ando@gmail.com>
2013-12-31 12:13:58 +0900

    Sec 3.4 Finish

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+
   end
 
   describe "Help pages" do
@@ -13,6 +19,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+
+    it "should have the title 'Help" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About pages" do
@@ -20,5 +31,11 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
+
+    it "should have the title 'About" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+    
   end
 end
