c9e81b96fc8a71c5b00f39d2a6e3a29e4cff68b6
T.Ando <taizo.ando@gmail.com>
2014-01-01 19:52:38 +0900

    Sec5.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
 
   describe "Home page" do
     it "should have the content 'Sample App" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('Home')
     end
 
@@ -24,36 +24,36 @@ describe "Static pages" do
 
   describe "Help pages" do
   	it "should have the content 'Help" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
 
     it "should have the title 'Help" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About pages" do
   	it "should have the content 'About Us" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
+  		visit about_path
+      expect(page).to have_content('About Us')
   	end
 
     it "should have the title 'About" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
   
   describe "Contact Pages" do
     it "should have the content 'Contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
