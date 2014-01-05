362fa6b5ed57eedf72526c2891023e2ff32dba82
T.Ando <taizo.ando@gmail.com>
2014-01-05 21:57:53 +0900

    sec6 fin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(nema: "takenoko yabu", email: "take@yabu.com") }
+	before { @user = User.new(nema: "takenoko yabu", email: "take@yabu.com",
+					 password: "takenoko", password_confirmation: "takenoko") }
 	subject { @user }
 
 	it { should respond_to(:nema) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -17,6 +21,19 @@ describe User do
 
 	describe "when email is not present" do
 		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(nema: "takenoko yabu", email: "take@yabu.com",
+					 password: " ", password_confirmation: " ")
+		end
+		it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
 		it { should_not be_valid }
 	end
 
@@ -55,5 +72,26 @@ describe User do
 				expect(@user).to be_valid
 			end
 		end
+	end
+
+	describe "return_value of authenticate method" do
+		before{ @user.save }
+		let(:found_user) { User.find_by(email: @user.email) }
+
+		describe "with valid password" do
+			it { should eq found_user.authenticate(@user.password) }
+		end
+
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+			it { should_not eq user_for_invalid_password }
+			specify { expect(user_for_invalid_password).to be_false}
+		end
+	end
+
+	describe "when the password is too short" do
+		before { @user.password = @user.password_confirmation = 'a'*5 }
+		it { should_not be_valid }
 	end
 end
