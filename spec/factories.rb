5ebba57aa7bee4b75dfcd3a3f6c87aa6c53dd3dd
T.Ando <taizo.ando@gmail.com>
2014-01-06 00:58:55 +0900

    sec7.1 fin

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 	factory :user do
-		name     "Michael Hartl"
+		nema     "Michael Hartl"
 		email    "michael@example.com"
 		password "foobar"
 		password_confirmation "foobar"
